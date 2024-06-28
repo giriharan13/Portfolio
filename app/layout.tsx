191d4b48cc420c0c3e6d25cf1ba93e109c9e3385
giriharan13 <lionking0304channel@gmail.com>
2024-06-28 22:02:17 +0530

    Made some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import Header from '@/components/Header'
 import './globals.css'
-import { Dosis } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
+import ActiveSectionContextProvider from '@/context/ActiveSectionContextProvider'
+import { Toaster } from 'react-hot-toast'
+import ThemeSwitch from '@/components/ThemeSwitch'
 
-const dosis = Dosis({ subsets: ['latin'] })
+const open_sans = Open_Sans({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Giriharan',
@@ -15,14 +18,24 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={`${dosis.className} text-violet-600`}>
-        <div className="absolute h-[5000px] w-full bg-gradient-to-b from-[#ffffff] to-[#f3e8ff]">
+    <html lang="en" className="!scroll-smooth">
+      <head>
+      <script src="https://code.iconify.design/2/2.0.4/iconify.min.js"></script>
+      </head>
+      <body className={`${open_sans.className} flex flex-col relative text-black`}>
+        <div className="absolute flex -z-10 h-full w-full bg-gradient-to-b from-[#ffffff] to-[#DBE2EF] dark:from-[#112D4E] dark:to-[#112D4E] dark:text-white dark:text-opacity-90">
 
         </div>
-        <Header/>
-        {children}
+        <ActiveSectionContextProvider>
+          <Header/>
+          {children}
+
+          <Toaster position='top-right'/>
+        </ActiveSectionContextProvider>
+        <ThemeSwitch/>
+        
         </body>
+
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+import Contact from "@/components/Contact";
+import Education from "@/components/Education";
+import Expertise from "@/components/Expertise";
 import Intro from "@/components/Intro";
+import Projects from "@/components/Projects";
+import SectionDivider from "@/components/SectionDivider";
 
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col items-center justify-center">
       <Intro/>
+      <SectionDivider/>
+      <Projects/>
+      <Expertise/>
+      <Education/>
+      <Contact/>
     </main>
   )
 }
