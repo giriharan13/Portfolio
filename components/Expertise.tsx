13619d9642a8c4a31b9dc5d4be3eb49071444744
giriharan13 <lionking0304channel@gmail.com>
2024-06-29 08:02:12 +0530

    Fixed alignment issue is smaller devices

--- components/Expertise.tsx
+++ components/Expertise.tsx
@@ -33,7 +33,7 @@ function Expertise() {
                 expertiseData.map((expertise,index)=>(
                   <li className="flex flex-col items-center" key={index}>
                     <h3 className='font-light text-xl text-center mb-10 dark:text-white text-[#112D4E]'>{expertise.title}</h3>
-                    <ul className='flex flex-wrap items-center justify-center gap-10 '>
+                    <ul className='flex flex-wrap items-center justify-center px-4'>
                         {
                             expertise.items.map((item,index2)=>(
                                 <motion.li key={index2}
@@ -44,8 +44,8 @@ function Expertise() {
                                         once:true
                                     }}
                                     custom={index2}>
-                                        <div className='flex flex-col items-center hover:scale-125 transition gap-2'>
-                                            <Icon icon={item.icon} className='h-20 w-20 bg-transparent'></Icon>
+                                        <div className='flex flex-col p-5 items-center hover:scale-125 transition gap-2'>
+                                            <Icon icon={item.icon} className='h-16 w-16 sm:h-20 sm:w-20 bg-transparent'></Icon>
                                             <p className='dark:text-white dark:text-opacity-90'>{item.name}</p>
                                         </div>
                                 </motion.li>
